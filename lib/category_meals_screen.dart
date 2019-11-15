6c6245edac2648ba9f2c110a4634a80bd321915a
Damian Bauer <damianbauer@MacBook-Pro-Damian.local>
2019-11-15 21:36:34 +0100

    1.0

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:meals/dummy-data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/categories-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen({this.categoryId, this.categoryTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(itemBuilder: (ctx, index) {
+        return Text(categoryMeals[index].title);
+      },itemCount: categoryMeals.length,),
+    );
+  }
+}
